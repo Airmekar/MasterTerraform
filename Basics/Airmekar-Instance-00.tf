f885b4a3bf13353fdda1e6c499d8e9efb1fd7eac
Harris <Airmekar@Outlook.com>
2021-01-22 13:44:17 -0500

    added key Pair

--- Basics/Airmekar-Instance-00.tf
+++ Basics/Airmekar-Instance-00.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "AirNV-WebServer-00" {
   #Redhat ami in North Virginia or you can use this amazon linux 2 "ami-0be2609ba883822ec"
   ami = "ami-096fda3c22c1c990a"
   instance_type = "t2.micro"
+  key_name ="LabWrk_Linux_KeyPair-01"
 
   tags = {
     Name = "AirNV-WebServer-00"
